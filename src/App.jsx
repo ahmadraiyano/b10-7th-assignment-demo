c882dfd1121dc3817dc6e0888fd58307b73f8ded
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2024-10-26 09:09:12 +0600

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Players from './components/Players/Players'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
-      <h1>assignment</h1>
-      
+      <Header></Header>
+      <Players></Players>
     </>
   )
 }
